6af61125504684e4adced0921dbc8ff549b2ea00
Saurabh Singh <115262830+saurabhbhuvi@users.noreply.github.com>
2025-06-07 18:39:09 +0000

    variables and data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234
+let accountEmail = "saurabh@gmail.com"
+var accountPassword = "sau123" 
+//  not  to use VAR because of block and fucntional scope issue 
+
+let accountState;
+// value not defined
+
+accountCity = "London"
+// not to use 
+
+ accountEmail = "saurabh123@gmail.com"
+ accountPassword = "sau1234545"
+
+accountCity = "dc"
+// accountID = 34; not allowed
+
+console.log(accountId);
+/*  ( // and /* )are used to comment out */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+// to print all the values we use table instead of writting again and again console.log( ) along with the array
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS Code as newer version
+
+console.log(6 + 9)
+
+console.log("saurabh")
+
+let name = "saurabh"
+let age = 22
+let isLoggedIn = true
+let state;   
+
+// number => 2 ^ 53
+//  bigint
+// string => ""   
+// null => its a value  and also an {OBJECT=> ( typeof null)}
+//undefined => 
+// symbol => unique
+
+
+
+//object 
+
+console.log(typeof "saurabh");
+console.log(typeof 21);
+// typeof is used to identity what it is , which data type it is 
+
+
+console.log(typeof undefined ); // output= undefined
+console.log(typeof  null);  //object
